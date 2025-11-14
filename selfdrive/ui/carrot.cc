fbbd2b0ff098d133007ecc8563a30e3490c4c3b1
jixiexiaoge <zhudongjer@outlook.com>
2025-11-14 20:15:36 +0800

    UI

--- selfdrive/ui/carrot.cc
+++ selfdrive/ui/carrot.cc
@@ -224,8 +224,8 @@ static inline void fill_rect(NVGcontext* vg, const Rect1& r, const NVGcolor* col
     if (stroke_width > 0) {
         nvgStrokeWidth(vg, stroke_width);
         if (stroke_color) nvgStrokeColor(vg, *stroke_color);
-		else nvgStrokeColor(vg, nvgRGB(0, 0, 0));   
-        nvgStroke(vg);                         
+		else nvgStrokeColor(vg, nvgRGB(0, 0, 0));
+        nvgStroke(vg);
     }
 }
 
@@ -570,6 +570,7 @@ protected:
     }
 };
 class PathEndDrawer : ModelDrawer {
+    Q_OBJECT
 private:
     QPointF path_end_left_vertex;
     QPointF path_end_right_vertex;
@@ -817,7 +818,7 @@ public:
             ui_draw_line2(s, px, py, 7, &pcolor, nullptr, 3.0f);
         }
         if (isLeadDetected()) {
-            NVGcolor radar_stroke = COLOR_BLUE;            
+            NVGcolor radar_stroke = COLOR_BLUE;
             if (lead_two_status > 0) {
               radar_stroke = COLOR_OCHRE;
               int path_width2 = lead_two_xr - lead_two_xl;
@@ -881,6 +882,7 @@ public:
 };
 
 class LaneLineDrawer : ModelDrawer {
+    Q_OBJECT
 private:
     float lane_line_probs[4];
     float road_edge_stds[2];
@@ -957,6 +959,7 @@ public:
     }
 };
 class TurnInfoDrawer : ModelDrawer {
+    Q_OBJECT
 private:
     int icon_size = 256;
     int xSpdLimit = 0;
@@ -1214,7 +1217,7 @@ public:
             return -1;
         }
         const auto carrot_man = sm["carrotMan"].getCarrotMan();
-          
+
         active_carrot = carrot_man.getActiveCarrot();
 
         if (active_carrot > 1) {
@@ -1238,7 +1241,7 @@ public:
           szSdiDescr = QString::fromStdString(carrot_man.getSzSdiDescr());
           szPosRoadName = QString::fromStdString(carrot_man.getSzPosRoadName());
           szTBTMainText = QString::fromStdString(carrot_man.getSzTBTMainText());
-          
+
         }
         else {
           //xTurnInfo = -1;
@@ -1267,6 +1270,7 @@ public:
 bool _right_blinker = false;
 bool _left_blinker = false;
 class DesireDrawer : ModelDrawer {
+    Q_OBJECT
 protected:
     int icon_size = 256;
     int blinker_timer = 0;
@@ -1341,6 +1345,7 @@ public:
 };
 
 class BlindSpotDrawer : ModelDrawer{
+    Q_OBJECT
 protected:
     QPolygonF lane_barrier_vertices[2];
 
@@ -1427,6 +1432,7 @@ public:
 };
 
 class PathDrawer : ModelDrawer {
+    Q_OBJECT
 private:
     QPolygonF track_vertices;
     int     show_path_mode = 13;
@@ -2028,7 +2034,7 @@ public:
               int max_z = lane_lines[2].getZ().size();
               float z_offset = 0.0;
               foreach(const QString & pair, pairs) {
-                QStringList xy = pair.split(",");  // ","로 x와 y 구분                
+                QStringList xy = pair.split(",");  // ","로 x와 y 구분
                 if (xy.size() == 3) {
                   //printf("coords = x: %.1f, y: %.1f, d:%.1f\n", xy[0].toFloat(), xy[1].toFloat(), xy[2].toFloat());
                   float x = xy[0].toFloat();
@@ -2301,7 +2307,7 @@ public:
     void drawHud(UIState* s) {
         int show_device_state = params.getInt("ShowDeviceState");
         blink_timer = (blink_timer + 1) % 16;
-        disp_timer = (disp_timer + 1) % 64; 
+        disp_timer = (disp_timer + 1) % 64;
         nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BOTTOM);
 
         int x = 140;// 120;
@@ -2850,7 +2856,7 @@ void ui_draw(UIState *s, ModelRenderer* model_renderer, int w, int h) {
   int path_x = drawPathEnd.getPathX();
   int path_y = drawPathEnd.getPathY();
   drawDesire.draw(s, path_x, path_y - 135);
-  
+
 
   drawPlot.draw(s);
 
